317e254feb1ed78148562df2b365fd260a73530a
vimal473 <vimaljparmar1643@gmail.com>
2026-09-22 12:21:03 +0530

    Add files via upload

---
+++ positive nagative.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number:"))
+
+if num >= 0:
+    if num == 0:
+        print("The number is zero")
+    else:
+            print("The number is positive")
+else:
+                print("The number is nagative")
