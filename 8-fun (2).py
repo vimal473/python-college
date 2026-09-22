24ca28e94f0d74d21d16e7b92e20bcfb62fe7170
vimal473 <vimaljparmar1643@gmail.com>
2026-09-22 12:14:51 +0530

    Add files via upload

---
+++ 8-fun (2).py
@@ -0,0 +1,13 @@
+def fun():
+    print("Funcation call")
+def fun1(x,y,name):
+    print(x)
+    print(y, name)
+def fun3(*name):
+    print(*name);
+fun1(12,60,"vimal")
+fun()
+fun3("keval","jenish")
+
+
+
