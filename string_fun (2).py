6b0ea2fbc57e47b0fd2697a5808323890bd2c0f3
vimal473 <vimaljparmar1643@gmail.com>
2026-09-22 12:21:47 +0530

    Add files via upload

---
+++ string_fun (2).py
@@ -0,0 +1,10 @@
+string = "ABCD"
+string1 = "abcd GFS"
+print(string.lower())
+print(string1.upper())
+print(string.capitalize())
+print(string.count("C"))
+print(string1.strip("abcd"))
+print(string1.endswith("GFS"))
+print(string1.split(" "))
+print(string.isalpha())
