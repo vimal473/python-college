706a82fc7b5a1f6000e360c18ac4d08447e80eab
vimal473 <vimaljparmar1643@gmail.com>
2026-09-22 12:14:29 +0530

    Add files via upload

---
+++ 7-dic (2).py
@@ -0,0 +1,19 @@
+dic = {
+   
+    'name':"raj",
+     'age':20
+        }
+
+print("dictionary items:",dic.items())
+print("dictionary keys:",dic.keys())
+print("dictionary values:",dic.values())
+dic['age']=50
+dic['name']='jenish'
+last_item=dic.pop('age')
+
+
+
+
+last_item=dic.clear()
+print(dic)
+
